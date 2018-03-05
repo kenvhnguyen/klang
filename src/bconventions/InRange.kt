4322584e4c35245cfacadd3f2b621fb0df27f091
Ken Nguyen <ken.nguyen@ing.nl>
2018-03-05 17:15:52 +0100

    in as an Infix for class with contains function

--- src/bconventions/InRange.kt
+++ src/bconventions/InRange.kt
@@ -1,8 +1,15 @@
 package bconventions
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+/**
+ * Any collection or another entity e.g DateRange below that defines the 'contains' method
+ * can use the 'in' keyword as an infix operator to check if a value belongs to a range.
+ * */
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+    operator fun contains(d: MyDate): Boolean {
+        return d > start && d < endInclusive
+    }
+}
 
 fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
-    //return date in DateRange(first, last)
-    return false
+    return date in DateRange(first, last)
 }
--- src/bconventions/TestConventions.kt
+++ src/bconventions/TestConventions.kt
@@ -14,4 +14,19 @@ class TestConventions {
         val second = MyDate(2014, 7, 11)
         Assert.assertTrue(first > second)
     }
+
+    // Test InRange.kt
+    fun doTest(date: MyDate, first: MyDate, last: MyDate, shouldBeInRange: Boolean) {
+        val message = "${date} should${if (shouldBeInRange) "" else "n't"} be in ${DateRange(first, last)}"
+        Assert.assertEquals(message, shouldBeInRange, checkInRange(date, first, last))
+    }
+    @Test fun `test if a date is in range`() {
+        doTest(MyDate(2018, 3, 22), MyDate(2018, 1, 1), MyDate(2019, 1, 1), shouldBeInRange = true)
+    }
+    @Test fun `test if a date is before a range`() {
+        doTest(MyDate(2017, 3, 22), MyDate(2018, 1, 1), MyDate(2019, 1, 1), shouldBeInRange = false)
+    }
+    @Test fun `test if a date is after a range`() {
+        doTest(MyDate(2019, 3, 22), MyDate(2018, 1, 1), MyDate(2019, 1, 1), shouldBeInRange = false)
+    }
 }
